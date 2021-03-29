adf5dfd9c759e36b9bf869070fd4c4261f4fb07f
sieugene <sieugene@mail.ru>
2021-03-29 20:42:35 +0300

    Add base endpoint

--- Services/index.ts
+++ Services/index.ts
@@ -2,12 +2,17 @@ import { Article, Articles } from "./../Schemas/ArticleSchema";
 import staticJSON from "../data/index.json";
 const isBuild = process.env.NODE_ENV === "production";
 
+const base =
+  process.env.NODE_ENV === "production"
+    ? "https://genesix-task.vercel.app/"
+    : "http://localhost:3000";
+
 export const getAllArticles = async (): Promise<Articles> => {
   if (isBuild) {
     return new Promise((resolve) => resolve(staticJSON as Articles));
   } else {
     try {
-      const response = await fetch("http://localhost:3000/api/articles");
+      const response = await fetch(`${base}/api/articles`);
       const data: Promise<Articles> = await response.json();
       return data;
     } catch (error) {
@@ -18,7 +23,7 @@ export const getAllArticles = async (): Promise<Articles> => {
 
 export const getArticle = async (id: string): Promise<Article> => {
   try {
-    const response = await fetch(`http://localhost:3000/api/articles?id=${id}`);
+    const response = await fetch(`${base}/api/articles?id=${id}`);
     const data: Promise<Article> = await response.json();
     return data;
   } catch (error) {
