c77d46e24a0264745ba2cee9cedd1a58f1969956
sieugene <sieugene@mail.ru>
2021-03-29 20:38:14 +0300

    Fix build

--- Services/index.ts
+++ Services/index.ts
@@ -1,12 +1,18 @@
 import { Article, Articles } from "./../Schemas/ArticleSchema";
+import staticJSON from "../data/index.json";
+const isBuild = process.env.NODE_ENV === "production";
 
 export const getAllArticles = async (): Promise<Articles> => {
-  try {
-    const response = await fetch("http://localhost:3000/api/articles");
-    const data: Promise<Articles> = await response.json();
-    return data;
-  } catch (error) {
-    return error;
+  if (isBuild) {
+    return new Promise((resolve) => resolve(staticJSON as Articles));
+  } else {
+    try {
+      const response = await fetch("http://localhost:3000/api/articles");
+      const data: Promise<Articles> = await response.json();
+      return data;
+    } catch (error) {
+      return error;
+    }
   }
 };
 
